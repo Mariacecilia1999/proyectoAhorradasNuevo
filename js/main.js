f5d72dba7b88dec57470e36f6057ccdfe46ce802
Mariacecilia1999 <conticecilia199@gamil.com>
2023-01-21 20:13:35 -0300

    Ocultando y  mostrando secciones con javascript

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+
+/*****Ocultando y mostrando secciones******/
+
+const $ = selector => document.querySelector(selector)
+
+
+/*Capturo botones del menú*/
+const botonBalance = $('.boton-balance')
+const botonCategorias = $('.boton-categorias')
+const botonReportes = $('.boton-reportes')
+
+
+//Capturo secciones mediante id
+const balance = $('#balance')
+const categoria = $('#categoria')
+const reportes = $('#reportes')
+
+
+/*Funciones ocultando/mostrando secciones*/
+
+
+let mostrarSeccionBalance = () =>{
+   balance.classList.remove('oculto')
+   categoria.classList.add('oculto')
+   reportes.classList.add('oculto')
+}
+
+let mostrarSeccionCategoria = () =>{
+   categoria.classList.remove('oculto')
+   balance.classList.add('oculto')
+   reportes.classList.add('oculto')
+}
+
+let mostrarSeccionReportes = () =>{
+   reportes.classList.remove('oculto')
+   balance.classList.add('oculto')
+   categoria.classList.add('oculto')
+}
+
+
+
+/*Validando click de botones*/
+botonBalance.addEventListener('click', mostrarSeccionBalance)
+botonCategorias.addEventListener('click', mostrarSeccionCategoria)
+botonReportes.addEventListener('click', mostrarSeccionReportes)
